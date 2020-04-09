3ff3412552f10333d0f8fda31c28245c38d28a38
Victor Loures <victorloures10@gmail.com>
2020-04-09 09:28:47 -0300

    Criei os metodos de lista na classe usuario

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 
 require_once("config.php");
+//carrega um usuario
+//$usuario = new Usuario();
+//$usuario->loadById(10);
+//echo $usuario;
 
+//Carrega uma lista de usuarios 
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios pelo login
+//$busca = Usuario::search("o");
+//echo json_encode($busca);
+
+//Carrega um usuário usando login e senha
 $usuario = new Usuario();
-
-$usuario->loadById(10);
-
+$usuario->login("Victor", "1003030303");
 echo $usuario;
 
 
